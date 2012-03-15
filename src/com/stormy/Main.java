93d73ff538f7a4c4fac9b8a3e94dce92a1a3ac54
Cory Virok <coryvirok@gmail.com>
2012-03-15 14:30:46 -0700

    simple UDP server on 9876

--- src/com/stormy/Main.java
+++ src/com/stormy/Main.java
@@ -1,15 +1,22 @@
 package com.stormy;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.HashMap;
 import java.util.Map;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.testing.TestWordSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
@@ -19,9 +26,10 @@ public class Main {
 
 	public static class TestBolt extends BaseRichBolt {
 		OutputCollector _collector;
-		
+
 		@Override
-		public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+		public void prepare(Map conf, TopologyContext context,
+				OutputCollector collector) {
 			_collector = collector;
 		}
 
@@ -35,19 +43,71 @@ public class Main {
 			declarer.declare(new Fields("word"));
 		}
 	}
-	
+
+	public static class SimpleUDPServerSpout extends BaseRichSpout {
+		DatagramSocket _server;
+		SpoutOutputCollector _collector;
+		byte[] _receiveData = new byte[1024];
+		byte[] _sendData = new byte[1024];
+
+		@Override
+		public void open(Map conf, TopologyContext context,
+				SpoutOutputCollector collector) {
+
+			if (this._server == null) {
+				try {
+					this._server = new DatagramSocket(9876);
+				} catch (SocketException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			this._collector = collector;
+		}
+
+		@Override
+		public void nextTuple() {
+			DatagramPacket receivePacket = new DatagramPacket(
+					this._receiveData, this._receiveData.length);
+			try {
+				this._server.receive(receivePacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			String line = new String(receivePacket.getData());
+			System.out.println("RECEIVED: " + line);
+			this._collector.emit(new Values(line));
+
+		}
+
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("line"));
+		}
+
+		@Override
+		public Map<String, Object> getComponentConfiguration() {
+			Map<String, Object> ret = new HashMap<String, Object>();
+			ret.put(Config.TOPOLOGY_MAX_TASK_PARALLELISM, 1);
+			return ret;
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception {
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("word", new TestWordSpout(), 10);
-        builder.setBolt("exclaim1", new TestBolt(), 3).shuffleGrouping("word");
-	
-        Config conf = new Config();
-        conf.setDebug(true);
-        
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("test", conf, builder.createTopology());
-        Utils.sleep(10000);
-        cluster.killTopology("test");
-        cluster.shutdown();
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("line", new SimpleUDPServerSpout(), 1);
+		builder.setBolt("exclaim1", new TestBolt(), 3).shuffleGrouping("line");
+
+		Config conf = new Config();
+		conf.setDebug(true);
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("test", conf, builder.createTopology());
+		Utils.sleep(1000000);
+		cluster.killTopology("test");
+		cluster.shutdown();
 	}
 }
