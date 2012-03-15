4da637093f1ffba3f80972bfbe198c12c1326e07
Cory Virok <coryvirok@gmail.com>
2012-03-15 15:52:57 -0700

    might have fixed

--- src/com/stormy/Main.java
+++ src/com/stormy/Main.java
@@ -1,6 +1,8 @@
 package com.stormy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -96,47 +98,56 @@ public class Main {
 		}
 
 	}
-	
-	private static void setLatLon(String url, float lat, float lon) {
-		try {
-		    URL myURL = new URL(url + "?lat=" + lat + "&long=" + lon);
-		    URLConnection myURLConnection = myURL.openConnection();
-		    myURLConnection.connect();
-		} 
-		catch (Exception e) { 
-		    System.err.println("Error: " + e);
-		}
 
+	private static void setLatLon(String url, float lat, float lon) {
+		URLConnection myURLConnection;
+		try {
+			URL myURL = new URL(url + "?lat=" + lat + "&long=" + lon);
+			myURLConnection = myURL.openConnection();
+			myURLConnection.connect();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					myURLConnection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+		} catch (Exception e) {
+			System.err.println("Error: " + e);
+		}
 	}
-	
+
 	public static class IPParserBolt extends BaseRichBolt {
 		OutputCollector _collector;
-		
+
 		@Override
-		public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+		public void prepare(Map conf, TopologyContext context,
+				OutputCollector collector) {
 			_collector = collector;
 		}
-		
+
 		@Override
 		public void execute(Tuple tuple) {
 			String[] data = tuple.getString(0).split(" ");
 			for (int i = 0; i < data.length; i++) {
-				if (data[i].matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")) {
+				if (data[i]
+						.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")) {
 					_collector.emit(tuple, new Values(data[i]));
 				}
 			}
 		}
-		
+
 		@Override
 		public void declareOutputFields(OutputFieldsDeclarer declarer) {
 			declarer.declare(new Fields("ip"));
 		}
 	}
-	
-    public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws Exception {
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setSpout("line", new SimpleUDPServerSpout(), 1);
-		builder.setBolt("exclaim1", new IPParserBolt(), 3).shuffleGrouping("line");
+		builder.setBolt("exclaim1", new IPParserBolt(), 3).shuffleGrouping(
+				"line");
 
 		Config conf = new Config();
 		conf.setDebug(true);
